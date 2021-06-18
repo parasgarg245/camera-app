31bf4813b0aa718735f0f8b1a74f80463972f504
Paras Garg <parasgarg245@gmail.com>
2021-06-18 17:27:46 +0530

    completed project

--- script.js
+++ script.js
@@ -1,172 +1,130 @@
 let video = document.querySelector("video");
-      let vidbtn=document.querySelector('button#record')
-      let capbtn=document.querySelector('button#capture')
-      
-      let constraints = {
-        video: true, ////////yha audio b true kar skte basically capability btayga
-        audio:true
-      };
-      
-      let body=document.querySelector('body')
-      
-      let filters=document.querySelectorAll('.filters');
-      let zoomin=document.querySelector('.zoomin')
-      let zoomout=document.querySelector('.zoomout')
-      
-      let mediaRecorder;
-      let isRecording=false;
-      let chunks=[]
-      
-      
-      let minzoom=1;
-      let maxzoom=3;
-      let currzoom=1;
-      
-      
-      let filter=""  //////canvas par use kar paye image par colour dalne k lia
-      for(let i=0;i<filters.length;i++){
-          filters[i].addEventListener('click',(e)=>{
-            filter=e.currentTarget.style.backgroundColor;
-            removeFilter();
-            addFilter(filter);
-          })
-      }
-      
-      
-      
-      zoomin.addEventListener('click',()=>{
-        if(currzoom<maxzoom){
-          currzoom++;
-          video.style.transform=`scale(${currzoom})`
-        }
-        
-      })
-      
-      zoomout.addEventListener('click',()=>{
-        if(currzoom>minzoom){
-          currzoom--;
-          video.style.transform=`scale(${currzoom})`
-          
-        }
-        
-      })
-      
-      
-      
-      
-      vidbtn.addEventListener('click',()=>{
-        let innerbtn=vidbtn.querySelector('div')
-        if(isRecording){
-          mediaRecorder.stop()
-          isRecording=false;
-          innerbtn.classList.remove('record-animation') 
-    
-        }else{
-          filter=""
-          removeFilter()
-          mediaRecorder.start()
-          isRecording=true;
-          innerbtn.classList.add('record-animation')
-          currzoom=1
-          video.style.transform=`style(${currzoom})`
-        }
-      })
-      
-      
-      capbtn.addEventListener('click',()=>{
-      
-        let innerbtn=capbtn.querySelector('div')
-        innerbtn.classList.add('capture-animation')
-        
-        setTimeout(function(){
-          innerbtn.classList.remove('capture-animation')
-        },500)
-        capture()
-      })
-      
-      //navigator b ek object hai
-      navigator.mediaDevices // mediadevice naviagator ka child object hai jo humko access deta hai video bnane ka
-      
-        .getUserMedia(constraints) ////////function hai jo promise dega arr constrainsts se hum ye bta pare hai ki vo vala device chaia jisse video mil jaye
-        .then(function (mediaStream) {
-          video.srcObject = mediaStream; // yha video k src mai mediastram daldia jo media stream ek url dega
-          let options = { mimeType: "video/webm; codecs=vp9" };
-          
-          mediaRecorder=new MediaRecorder(mediaStream,options)
-        
-          mediaRecorder.addEventListener('dataavailable',function(e){
-         
-            chunks.push(e.data)
-          
-          })
-          
-          mediaRecorder.addEventListener('stop',function(e){
-         
-            let blob=new Blob(chunks,{type:"video/mp4"}) /// large raw data 
-            
-            addMedia('vid',blob)
-            chunks=[]
-            
-            let url=URL.createObjectURL(blob)
-            console.log(url)
-            let a=document.createElement('a')
-            a.href=url
-            a.download='video.mp4'
-            a.click()
-            a.remove()
-          })
-        
-        
-        }); 
-        
-        
-        
-        
-        function capture(){
-        
-            let c=document.createElement('canvas')
-            
-            c.width=video.videoWidth
-            c.height=video.videoHeight
-            
-            let ctx=c.getContext('2d')
-            
-            ctx.translate(c.width/2,c.height/2)
-            ctx.scale(currzoom,currzoom)
-            ctx.translate(-c.width/2,-c.height/2)
-            
-            
-            ctx.drawImage(video,0,0)
-            if(filter!=""){
-              ctx.fillStyle=filter;
-              ctx.fillRect(0,0,c.width,c.height)
-            }
-            
-            
-            ///download karne k lia
-            let a=document.createElement('a')
-            a.download='image.png'
-            a.href=c.toDataURL()
-            
-            addMedia('img',c.toDataURL());
-            a.click()
-            a.remove()
-        
-        }
-        
-        
-        
-        function addFilter(filter){
-          let filterDiv=document.createElement('div');
-          filterDiv.classList.add('filter-div');
-          filterDiv.style.backgroundColor=filter
-          body.appendChild(filterDiv)
-        }
-        
-        
-        function removeFilter(){
-          
-          let filterDiv=document.querySelector('.filter-div');
-          if(filterDiv!=null)
-          filterDiv.remove();
-        }
+let vidBtn = document.querySelector("button#record");
+let body = document.querySelector("body")
+let capBtn = document.querySelector("button#capture");
+let filters = document.querySelectorAll(".filters")
+let zoomin = document.querySelector(".zoom-in");
+let zoomout = document.querySelector(".zoom-out")
+let gallerybtn = document.querySelector("#gallery");
+let constraints = { video: true, audio: false };
+let mediaRecorder;
+let isRecording = false;
+let chunks = [];
+
+let minzoom = 1;
+let maxzoom = 3;
+let currzoom = 1;
+
+gallerybtn.addEventListener("click" , function(){
+  location.assign("gallery.html")
+})
+let filter = "";
+
+for(let i =0 ; i < filters.length ; i++){
+  filters[i].addEventListener("click", function(e){
+
+    filter = e.currentTarget.style.backgroundColor;
+    removefilter();
+    applyfilter(filter);
+
+  })
+}
+zoomin.addEventListener("click" , function(){
+ let videocurrscale = video.style.transform.split("(")[1].split(")")[0];
+if(videocurrscale > maxzoom){
+  return;
+}else{
+  currzoom = Number(videocurrscale) + 0.1;
+  video.style.transform = `scale(${currzoom})`;
+}
+});
+zoomout.addEventListener("click" , function(){
+ 
+ if(currzoom > minzoom){
+  currzoom-= 0.1;
+  video.style.transform = `scale(${currzoom})`;
+ }
+ })
+vidBtn.addEventListener("click", function () {
+  let innerDiv = vidBtn.querySelector("div");
+
+  if (isRecording) {
+    mediaRecorder.stop();
+    isRecording = false;
+    innerDiv.classList.remove("record-animation");
+  } else {
+    mediaRecorder.start();
+    filter=""
+    removefilter();
+    isRecording = true;
+    innerDiv.classList.add("record-animation");
+  }
+});
+
+capBtn.addEventListener("click", function () {
+  let innerDiv = capBtn.querySelector("div");
+  innerDiv.classList.add("capture-animation");
+  setTimeout(function () {
+    innerDiv.classList.remove("capture-animation");
+  }, 500);
+  capture();
+});
+
+navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
+
+  video.srcObject = mediaStream; // yha video k src mai mediastram daldia jo media stream ek url dega
+  let options = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(mediaStream,options);
+
+  mediaRecorder.addEventListener("dataavailable", function (e) {
+    chunks.push(e.data);
+  });
+
+  mediaRecorder.addEventListener("stop", function () {
+    let blob = new Blob(chunks, { type: "video/mp4" });
+    addMedia("video" , blob)
+    chunks = [];
+
+    let url = URL.createObjectURL(blob);
+
+    // let a = document.createElement("a");
+    // a.href = url;
+    // a.download = "video.mp4";
+    // a.click();
+    // a.remove();
+  });
+});
+
+function capture() {
+  let c = document.createElement("canvas");
+  c.width = video.videoWidth;
+  c.height = video.videoHeight;
+  let ctx = c.getContext("2d");
+
+  ctx.translate(c.width/2 , c.height/2);
+  ctx.scale(currzoom , currzoom);
+  ctx.translate(-c.width/2 , -c.height/2);
+
+  ctx.drawImage(video, 0, 0);
+  if(filter!= ""){
+    ctx.fillStyle=filter;
+    ctx.fillRect(0,0,c.width , c.height)
+  }
+  // let a = document.createElement("a");
+  // a.download = "image.jpg";
+  // a.href = c.toDataURL();
+  addMedia("img" , c.toDataURL())
+  // a.click();
+  // a.remove();
+}
+function applyfilter(filterColor){
+let filterdiv= document.createElement("div")
+filterdiv.classList.add("filter-div");
+filterdiv.style.backgroundColor = filterColor;
+body.appendChild(filterdiv)
+}
+function removefilter(){
+  let filterdiv = document.querySelector(".filter-div")
+if(filterdiv) filterdiv.remove()
+
+}
